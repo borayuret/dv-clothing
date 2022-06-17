bc3321d938c08889575dbaa8f33bb83191c4f77a
Bora Yüret <borayuret@gmail.com>
2022-06-17 10:54:56 +0300

    firebase init

---
+++ src/util/firebase.js
@@ -0,0 +1,14 @@
+
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAJPiNk0cOdR4fcpevqO-F3s9UygqEKBNM",
+  authDomain: "dv-clothing-db-ac43c.firebaseapp.com",
+  projectId: "dv-clothing-db-ac43c",
+  storageBucket: "dv-clothing-db-ac43c.appspot.com",
+  messagingSenderId: "212779236500",
+  appId: "1:212779236500:web:05d84083a8d35276a00881"
+};
+
+
+const app = initializeApp(firebaseConfig);
